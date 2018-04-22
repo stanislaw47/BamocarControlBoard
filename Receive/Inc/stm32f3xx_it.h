bbc7b74e17a53c4401f5a7d47cca72d19d4a1447
stanislaw47 <stanislaw.cabala@gmail.com>
2018-04-22 22:27:34 +0200

    add data verification

--- Receive/Inc/stm32f3xx_it.h
+++ Receive/Inc/stm32f3xx_it.h
@@ -60,6 +60,7 @@ void USB_HP_CAN_TX_IRQHandler(void);
 void USB_LP_CAN_RX0_IRQHandler(void);
 void CAN_RX1_IRQHandler(void);
 void CAN_SCE_IRQHandler(void);
+void TIM1_UP_TIM16_IRQHandler(void);
 
 #ifdef __cplusplus
 }
