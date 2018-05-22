d0fe668f11944e5142ea43eded013293d81732be
stanislaw47 <stanislaw.cabala@gmail.com>
2018-05-22 14:51:52 +0200

    cleanup before first release

--- TrueStudioProject/Modules_inc/CAN_MC.h
+++ TrueStudioProject/Modules_inc/CAN_MC.h
@@ -4,116 +4,84 @@
 #ifndef CAN_MC_
 #define CAN_MC_
 
+//includes
 #include <string.h>
 #include "can.h"
+#include "FIFO.h"
 
-/*
- * Interface defines
- */
-#define CAN_DATA_LEN_TX 	3 		//iloœc bajtów danych
-#define CAN_DATA_LEN_RX 	6 		//iloœc bajtów danych odbieranych ze sterownika, zosta³a ustawiona poprzez NDrive
-#define CAN_ID_TX 			0x201 	//ID ramki wysy³anej do sterownika, wykorzystywane w filtrach
-#define CAN_ID_RX 			0x181 	//ID ramki odebranej ze sterownika, wykorzystywane w filtrach
-#define DATA_FREQ 			0x32 	//czêstotliwosc z jak¹ Bamocar bêdzie zwraca³ ramki danych [50ms]
-#define TxBufferLength 		0x08	//number of Tx struct frames
-#define RxBufferLength		0x01
+//defines
+// CAN configuration parameters
+#define CAN_DATA_LEN_TX 	3 		//number of bytes send to Bamocar
+#define CAN_DATA_LEN_RX 	6 		//number fo bytes received from Bamcoar, fixed via NDrive
+#define CAN_ID_TX 			0x201 	//send frame ID, used in filters
+#define CAN_ID_RX 			0x181 	//received frame ID, used in filters
 
-/*
- * Bamocar register defines
- */
-#define REG_READ 				0x3d 	//odczytywanie wartoœci rejestru
-#define REG_REPLY_NOW 			0x00 	//rozkaz natychmiastowej odpowiedzi
-#define REG_REPLY_STOP 			0xff 	//rozkaz zakoñczenia wysy³ania cyklicznego
+//registers configuration parameters
+#define DATA_FREQ 			0x32 	//cyclic data frequency, in miliseconds [50]
+#define READ 				0x3d 	//value for reading registers
+#define REPLY_NOW 			0x00 	//value for immediately reply
+#define REPLY_STOP 			0xff 	//value for stop sending cyclic data
 
-#define REG_CUR_LIM			0x46 	//rejestr do odczytu pr¹du
-#define REG_TORQUE 			0x90 	//rejestr do odczytu momentu obrotowego
-#define REG_BUS_DC			0xeb 	//rejestr do odczytu napiêcia magistrali
+//error status defines
+#define CAN_FILTER_ERROR	1		//bit number in CAN_MC_Status which indicate filter initialization error
+#define CAN_RX_ERROR		2		//bit number in CAN_MC_Status which indicate frame receive error
+#define CAN_TX_ERROR		3		//bit number in CAN_MC_Status which indicate frame transmit error
+#define MC_READY			4		//bit number in CAN_MC_Status which indicate that device is not ready error
+#define MC_FRG				5		//bit number in CAN_MC_Status which indicate is FRG set error
+#define LOCKED				6		//bit number in CAN_MC_Status which indicate whther data are locked or not
 
-/*
- * Error status defines
- * Used for CAN_MC_Status bit modification
- */
-#define CAN_MC_INIT_ERROR	0x01
-#define CAN_MC_OVERRUN		0x02
-#define CAN_MC_RX_ERROR		0x04
-#define CAN_MC_FILT_ERROR	0x08
-#define CAN_MC_TX_ERROR		0x10
-
-/*
- * Tx&Rx frames status defines
- * Used for CAN_MC_CAN_TxBuffer.Status and DataLocked bit modification and data assignment
- */
-#define TX_READY				0x00
-#define TX_FRG					0x01
-#define TX_ENABLE				0x02
-#define TX_CURRENT_LIMIT		0x03
-#define TX_TORQUE				0x04
-#define TX_VDC					0x05
-
-#define RX_VDC					0x00
-
-/*
- * Data fields
- */
-CanRxMsgTypeDef RxMessage1; 		//for CAN_FIFO0 data
-CanRxMsgTypeDef RxMessage2; 		//for CAN_FIFO1 data
-CAN_FilterConfTypeDef RxFilter; 	//Can filter configuration structure
-static uint8_t CAN_MC_Status;		//CAN_MC Error status field - 0 means no error
-static uint8_t CAN_MC_Locked;		//Locking object
-
-static struct __CAN_MC_TxBuffer
-{
-	CanTxMsgTypeDef Buffer[TxBufferLength];
-	uint16_t Status;
-
-} CAN_MC_TxBuffer;
-static struct __CAN_MC_InData
-{
-	uint16_t Data[RxBufferLength];
-} CAN_MC_InData;
-static struct __CAN_MC_InDataLocked
-{
-	uint16_t Data[RxBufferLength];
-	uint8_t Status;
-} CAN_MC_InDataLocked;
+//data configuration registers
+#define SPEED 				0x30 	//value of register to read speed
+#define SET_SPEED 			0x31	//value of register to write speed
+#define CURRENT 			0x20 	//value of register to read speed
+#define TORQUE 				0xa8 	//value of register to read speed
+#define SET_TORQUE 			0x90 	//value of register to write torque
+#define STATUS 				0x40 	//value of register to read status
+#define READY 				0xe2 	//value of register to read whether device is ready
+#define FRG 				0xe8 	//value of register to set FRG
+#define BUS_DC 				0xeb 	//value of register to read bus voltage
+#define MODE 				0x51 	//value of register to read status and switch on/off
+#define MOTOR_ENABLE 		0x00 	//value for turning on
+#define MOTOR_DISABLE 		0x04 	//value for turning off
 
 
-/*
- * Init and locking functions
- */
-void CAN_MC_Init(void); 			//CAN_MC_TxBuffer and filters initialisation
-void CAN_MC_Lock(void); 			//provides locking mechanism for data integrity
-void CAN_MC_Unlock(void); 			//function complementary to CAN_MC_Lock()
+CanTxMsgTypeDef TxMessage; //structure for trasmitted frame
+CanRxMsgTypeDef RxMessage; //structure for received fram
+CanRxMsgTypeDef RxMessage2; //for second queue
+CAN_FilterConfTypeDef RxFilter; //structure for filter
+fifo TxBuffer; //buffer for send frames
+//CanRxMsgTypeDef* FrameBuffer;
 
+static struct __CAN_MC_Data{
+	uint32_t Status;
+	uint16_t Speed;
+	uint16_t Current;
+	uint16_t Torque;
+	uint16_t Ready;
+	uint16_t Frg;
+	uint16_t BusDC;
+	uint32_t Others;
+}CAN_MC_Data, CAN_MC_DataLocked; //structure for data from Bamocar
 
-/*
- * Functions setting flags for CAN frames transmitting
- */
-void CAN_MC_CyclicDataEnable(void);
-void CAN_MC_TorqueCommand(uint16_t Torque);
-void CAN_MC_CurrentLimitCommand(uint16_t CurrentLimit);
-//for future implementation
-void CAN_MC_Disconnect(void);
-void CAN_MC_CyclicDataDisable(void);
-void CAN_MC_StopCommand(void);
-void CAN_MC_SpeedCommand(uint16_t Speed);
+static uint8_t CAN_MC_Status; //
 
+void CAN_MC_Init(CAN_HandleTypeDef *hpcan); //initialization fo frames and filters
+void CAN_MC_Connect(CAN_HandleTypeDef *hpcan); //checks whether device is ready and FRG set
+void CAN_MC_Disconnect(CAN_HandleTypeDef *hpcan); //turns drive off
+void CAN_MC_Transmit(CAN_HandleTypeDef *hpcan, uint8_t d1, uint8_t d2, uint8_t d3); //main sending function
+void CAN_MC_ReceiveCallback(CAN_HandleTypeDef *hpcan); //frame receive callback
+void CAN_MC_CyclicDataEnable(CAN_HandleTypeDef *hpcan); //turns on cyclic data sending by Bamocar
+void CAN_MC_CyclicDataDisable(CAN_HandleTypeDef *hpcan);//turns off cyclic data sending by Bamcoar
+void CAN_MC_Lock(); //handles Locked flag, starts reading data
+void CAN_MC_Unlock(); //handles Locked flag, finishes reading data
+uint16_t CAN_MC_GetSpeed(); //read value of speed
+uint16_t CAN_MC_GetTorque(); //read value of torque
+uint16_t CAN_MC_Current(); //read value of current
+uint16_t CAN_MC_GetReady(); //check whether device is ready
+uint16_t CAN_MC_GetFRG(); //read value of FRG
+uint16_t CAN_MC_GetBusDC(); //read value of bus voltage
+void CAN_MC_SpeedCommand(CAN_HandleTypeDef *hpcan, uint16_t data); //set speed
+void CAN_MC_TorqueCommand(CAN_HandleTypeDef *hpcan, uint16_t data); //set torque
+void CAN_MC_StopCommand(CAN_HandleTypeDef *hpcan); //stop motor
 
-/*
- * Functions returning data
- */
-uint8_t CAN_MC_GetBusVDC(); //odczyt napiêcia na magistrali
-//for future implementation
-uint8_t CAN_MC_GetSpeed(); //odczyt prêdkoœci
-uint8_t CAN_MC_GetTorque(); //odczyt momentu
-uint8_t CAN_MC_GetReady(); //odczyt gotowoœci
-uint8_t CAN_MC_GetFRG(); //odczyt FRG
-uint8_t CAN_MC_Current(); //odczyt pr¹du
-
-/*
- * Functions transmitting & receiving CAN frames
- */
-void CAN_MC_ReceiveCallback(void); //callback od odebrania ramki
-void CAN_MC_TransmitCallback(void); //callback od wyslania ramki
-
-#endif
+#endif //CAN_MC_
--- TrueStudioProject/Modules_src/CAN_MC.c
+++ TrueStudioProject/Modules_src/CAN_MC.c
@@ -1,137 +1,162 @@
-#include <CAN_MC.h>
-#include <string.h>
 #include "CAN_MC.h"
 
 
-void BCB_Transmit(CAN_HandleTypeDef *hpcan, uint8_t d1, uint8_t d2, uint8_t d3){
-	hpcan->pTxMsg->Data[0] = d1;
-	hpcan->pTxMsg->Data[1] = d2;
-	hpcan->pTxMsg->Data[2] = d3;
-	while(HAL_CAN_Transmit_IT(hpcan) != HAL_OK) //wysłanie ramki przez CANa
-		;
+void CAN_MC_Transmit(CAN_HandleTypeDef *hpcan, uint8_t d1, uint8_t d2, uint8_t d3){
+	CanTxMsgTypeDef Frame = *hpcan->pTxMsg;
+	Frame.Data[0] = d1;
+	Frame.Data[1] = d2;
+	Frame.Data[2] = d3;
+	fifo_push(&TxBuffer, &Frame);
 }
 
-
-
-void CAN_MC_Init(void){
-
-	/*
-	 * CAN_MC_TxBuffer initialisation
-	 */
-	uint8_t index;
-	for(index=0;index<TxBufferLength;index++)
-	{
-		CAN_MC_TxBuffer.Buffer[index].DLC=CAN_DATA_LEN_TX;
-		CAN_MC_TxBuffer.Buffer[index].StdId=CAN_ID_TX;
-		CAN_MC_TxBuffer.Buffer[index].IDE=CAN_ID_STD;
-	}
-
-	CAN_MC_TxBuffer.Buffer[TX_CURRENT_LIMIT].Data[0]=REG_CUR_LIM;
-
-	CAN_MC_TxBuffer.Buffer[TX_TORQUE].Data[0]=REG_TORQUE;
-
-	CAN_MC_TxBuffer.Buffer[TX_VDC].Data[0]=REG_READ;
-	CAN_MC_TxBuffer.Buffer[TX_VDC].Data[1]=REG_BUS_DC;
-	CAN_MC_TxBuffer.Buffer[TX_VDC].Data[2]=DATA_FREQ;
-
-	/*
-	 * Filters and Rx struct initialisation
-	 */
-	RxFilter.FilterNumber = 0;
-	RxFilter.FilterMode = CAN_FILTERMODE_IDMASK;
-	RxFilter.FilterMaskIdHigh = 0xFFFF;
-	RxFilter.FilterMaskIdLow = 0xFFFF;
-	RxFilter.FilterIdHigh = CAN_ID_RX << 5;
-	RxFilter.FilterIdLow = 0x00;
-	RxFilter.FilterFIFOAssignment = CAN_FIFO0;
-	RxFilter.FilterActivation = ENABLE;
-	RxFilter.FilterMode=CAN_FILTERSCALE_16BIT;
-	if (HAL_CAN_ConfigFilter(&hcan, &RxFilter) != HAL_OK)
-		SET_BIT(CAN_MC_Status,CAN_MC_FILT_ERROR);
-
-	/*
-	 * Rx structures address assignment
-	 */
-	hcan.pRxMsg = &RxMessage1;
-	hcan.pRx1Msg = &RxMessage2;
-	if (HAL_CAN_Receive_IT(&hcan,CAN_FIFO0) != HAL_OK)
-		SET_BIT(CAN_MC_Status,CAN_MC_RX_ERROR);
-}
-void CAN_MC_Lock()
-{
-	if(CAN_MC_Status)
-		SET_BIT(CAN_MC_Status,CAN_MC_OVERRUN);
-	else
-		CAN_MC_Locked = 1;
-}
-void CAN_MC_Unlock()
-{
-	uint8_t index=0;
-	for(index=0;index<RxBufferLength;index++)
-		if(READ_BIT(CAN_MC_InDataLocked.Status,1<<index))
-			CAN_MC_InData.Data[index]=CAN_MC_InDataLocked.Data[index];
-	CAN_MC_Locked = 0;
-}
-
-
-
-uint16_t BCB_GetBusDC()
-{
-	return CAN_MC_InData.Data[RX_VDC];
-}
-
-
-
-void CAN_MC_CyclicDataEnable(void)
-{
-	SET_BIT(CAN_MC_TxBuffer.Status,1<<TX_VDC);
-}
-void CAN_MC_TorqueCommand(uint16_t DataToSend)
-{
-	CAN_MC_TxBuffer.Buffer[TX_TORQUE].Data[1] = DataToSend % 256;
-	CAN_MC_TxBuffer.Buffer[TX_TORQUE].Data[2] = DataToSend >> 8 % 256;
-	SET_BIT(CAN_MC_TxBuffer.Status,1<<TX_TORQUE);
-}
-void CAN_MC_CurrentLimitCommand(uint16_t DataToSend)
-{
-	CAN_MC_TxBuffer.Buffer[TX_CURRENT_LIMIT].Data[1] = DataToSend % 256;
-	CAN_MC_TxBuffer.Buffer[TX_CURRENT_LIMIT].Data[2] = DataToSend >> 8 % 256;
-	SET_BIT(CAN_MC_TxBuffer.Status,1<<TX_CURRENT_LIMIT);
-}
-
-
-
-void CAN_MC_TransmitCallback(void)
-{
-	uint8_t index=0;
-	for(index=0;index<TxBufferLength;index++)
-	if(READ_BIT(CAN_MC_TxBuffer.Status,1<<index))
-	{
-		hcan.pTxMsg=&(CAN_MC_TxBuffer.Buffer[index]);
-		if(HAL_CAN_Transmit_IT(&hcan) != HAL_OK)
-			return;
-		else
-			CLEAR_BIT(CAN_MC_TxBuffer.Status,1<<index);
-	}
-}
-void CAN_MC_ReceiveCallback(void)
-{
-	switch(hcan.pRxMsg->Data[0])
-	{
-		case REG_BUS_DC:
-			if(CAN_MC_Locked)
-			{
-				CAN_MC_InDataLocked.Data[RX_VDC] = hcan.pRxMsg->Data[1];
-				CAN_MC_InDataLocked.Data[RX_VDC] |= hcan.pRxMsg->Data[1] << 8;
-				SET_BIT(CAN_MC_InDataLocked.Status,1<<RX_VDC);
-			}
-			else
-			{
-				CAN_MC_InData.Data[RX_VDC] = hcan.pRxMsg->Data[1];
-				CAN_MC_InData.Data[RX_VDC] |= hcan.pRxMsg->Data[2] << 8;
-			}
+void CAN_MC_ReceiveCallback(CAN_HandleTypeDef *hpcan){
+	switch(hpcan->pRxMsg->Data[0]){
+		case SPEED:
+			CAN_MC_Data.Speed = ((uint16_t)(hpcan->pRxMsg->Data[1]) << 8) | hpcan->pRxMsg->Data[2];
 			break;
+		case CURRENT:
+			CAN_MC_Data.Current = ((uint16_t)(hpcan->pRxMsg->Data[1]) << 8) | hpcan->pRxMsg->Data[2];
+			break;
+		case STATUS:{
+			CAN_MC_Data.Status = hpcan->pRxMsg->Data[3];
+			CAN_MC_Data.Status = (CAN_MC_Data.Status << 8) + hpcan->pRxMsg->Data[2];
+			CAN_MC_Data.Status = (CAN_MC_Data.Status << 8) + hpcan->pRxMsg->Data[1];
+			CAN_MC_Data.Status = (CAN_MC_Data.Status << 8) + hpcan->pRxMsg->Data[0];
+			break;}
+		case TORQUE:
+			CAN_MC_Data.Torque = ((uint16_t)(hpcan->pRxMsg->Data[1]) << 8) | hpcan->pRxMsg->Data[2];
+			break;
+		case READY:
+			CAN_MC_Data.Ready = ((uint16_t)(hpcan->pRxMsg->Data[1]) << 8) | hpcan->pRxMsg->Data[2];
+			break;
+		case FRG:
+			CAN_MC_Data.Frg = ((uint16_t)(hpcan->pRxMsg->Data[1]) << 8) | hpcan->pRxMsg->Data[2];
+			break;
+		case BUS_DC:
+			CAN_MC_Data.BusDC = ((uint16_t)(hpcan->pRxMsg->Data[1]) << 8) | hpcan->pRxMsg->Data[2];
+			break;
+		default:{  //other data
+			CAN_MC_Data.Others = hpcan->pRxMsg->Data[3];
+			CAN_MC_Data.Others = (CAN_MC_Data.Others << 8) + hpcan->pRxMsg->Data[2];
+			CAN_MC_Data.Others = (CAN_MC_Data.Others << 8) + hpcan->pRxMsg->Data[1];
+			CAN_MC_Data.Others = (CAN_MC_Data.Others << 8) + hpcan->pRxMsg->Data[0];
+			break;}
 	}
-	if (HAL_CAN_Receive_IT(&hcan,CAN_FIFO0) != HAL_OK)
-		SET_BIT(CAN_MC_Status,CAN_MC_RX_ERROR);
+}
+
+void CAN_MC_Init(CAN_HandleTypeDef *hpcan){
+	// initialize buffer for transmit frames
+	fifo_init(&TxBuffer);
+
+	// turn on CAN interrupts for queues 0 and 1
+	__HAL_CAN_ENABLE_IT(hpcan, CAN_IT_FMP0);
+	__HAL_CAN_ENABLE_IT(hpcan, CAN_IT_FMP1);
+
+	//configuration of transmit frame
+	TxMessage.DLC = CAN_DATA_LEN_TX;
+	TxMessage.RTR = CAN_RTR_DATA;
+	TxMessage.IDE = CAN_ID_STD; //standard ID version
+	TxMessage.StdId = CAN_ID_TX;
+	hpcan->pTxMsg = &TxMessage; //pass this structure to main structure of CAN handler
+
+	//configuration of frame filter
+	RxFilter.FilterNumber = 1;
+	RxFilter.FilterMode = CAN_FILTERMODE_IDMASK; //set filtering mode to mask, not list
+	RxFilter.FilterMode = CAN_FILTERSCALE_16BIT; //16 bits mask mode
+	RxFilter.FilterMaskIdHigh = 0xFFFF; //when mask is set to max, it doesn't matter
+	RxFilter.FilterMaskIdLow = 0xFFFF;
+	RxFilter.FilterIdHigh = CAN_ID_RX << 5; //CAN ID has only 11 bits, so it has to be moved by 5 bits
+	RxFilter.FilterIdLow = 0x00; //to work fine, it has to be set to zero
+	RxFilter.FilterFIFOAssignment = CAN_FIFO0;
+	RxFilter.FilterActivation = ENABLE; //activation of filter
+	if (HAL_CAN_ConfigFilter(hpcan, &RxFilter) != HAL_OK) //turn on filter and check whether it's fine
+		SET_BIT(CAN_MC_Status, 1<<CAN_FILTER_ERROR);
+
+	//configuration od receive frame
+	hpcan->pRxMsg = &RxMessage; //pass this structure to main structure of CAN handler
+	hpcan->pRx1Msg = &RxMessage2; //for second queue
+	if (HAL_CAN_Receive_IT(hpcan, CAN_FIFO0) != HAL_OK)
+		SET_BIT(CAN_MC_Status, 1<<CAN_RX_ERROR);
+}
+
+void CAN_MC_Connect(CAN_HandleTypeDef *hpcan){
+	// is device ready?
+	CAN_MC_Transmit(hpcan, READ, READY, REPLY_NOW);
+	if(hpcan->pRxMsg->Data[1] != 0x01 || hpcan->pRxMsg->Data[2] != 0x00 || hpcan->pRxMsg->Data[3] != 0x00)
+		SET_BIT(CAN_MC_Status, 1<<MC_READY);
+
+	//is FRG set?
+	CAN_MC_Transmit(hpcan, READ, FRG, REPLY_NOW);
+	if(hpcan->pRxMsg->Data[1] != 0x01 || hpcan->pRxMsg->Data[2] != 0x00 || hpcan->pRxMsg->Data[3] != 0x00)
+		SET_BIT(CAN_MC_Status, 1<<MC_FRG);
+
+	//lock drive
+//	CAN_MC_Transmit(hpcan, MODE, MOTOR_DISABLE, 0x00);
+
+	//enable drive
+	CAN_MC_Transmit(hpcan, MODE, MOTOR_ENABLE, 0x00);
+}
+
+void CAN_MC_CyclicDataEnable(CAN_HandleTypeDef *hpcan){
+	//send command to start cyclic data
+	CAN_MC_Transmit(hpcan, READ, SPEED, DATA_FREQ);
+	CAN_MC_Transmit(hpcan, READ, TORQUE, DATA_FREQ);
+	CAN_MC_Transmit(hpcan, READ, BUS_DC, DATA_FREQ);
+}
+
+void CAN_MC_CyclicDataDisable(CAN_HandleTypeDef *hpcan){
+	//send command to stop cyclic data
+	CAN_MC_Transmit(hpcan, READ, SPEED, REPLY_STOP);
+	CAN_MC_Transmit(hpcan, READ, TORQUE, REPLY_STOP);
+	CAN_MC_Transmit(hpcan, READ, BUS_DC, REPLY_STOP);
+}
+
+void CAN_MC_Disconnect(CAN_HandleTypeDef *hpcan){
+	//disnable of drive
+	CAN_MC_Transmit(hpcan, MODE, MOTOR_DISABLE, 0x00);
+}
+
+void CAN_MC_Lock(){
+	CAN_MC_DataLocked = CAN_MC_Data;
+	SET_BIT(CAN_MC_Status, 1<<LOCKED);
+}
+
+void CAN_MC_Unlock(){
+	CLEAR_BIT(CAN_MC_Status, 1<<LOCKED);
+}
+
+uint16_t CAN_MC_GetSpeed(){
+	return CAN_MC_Data.Speed;
+}
+
+uint16_t CAN_MC_GetReady(){
+	return CAN_MC_Data.Ready;
+}
+
+uint16_t CAN_MC_GetFRG(){
+	return CAN_MC_Data.Frg;
+}
+
+uint16_t CAN_MC_GetTorque(){
+	return CAN_MC_Data.Torque;
+}
+
+uint16_t CAN_MC_GetCurrent(){
+	return CAN_MC_Data.Current;
+}
+
+uint16_t CAN_MC_GetBusDC(){
+	return CAN_MC_Data.BusDC;
+}
+
+void CAN_MC_SpeedCommand(CAN_HandleTypeDef *hpcan, uint16_t data){
+	CAN_MC_Transmit(hpcan, SET_SPEED, data % 256, data >> 8 % 256);
+}
+
+void CAN_MC_TorqueCommand(CAN_HandleTypeDef *hpcan, uint16_t data){
+	CAN_MC_Transmit(hpcan, SET_TORQUE, data % 256, data >> 8 % 256);
+}
+
+void CAN_MC_StopCommand(CAN_HandleTypeDef *hpcan){
+	CAN_MC_Transmit(hpcan, SET_SPEED, 0x00, 0x00);
 }
